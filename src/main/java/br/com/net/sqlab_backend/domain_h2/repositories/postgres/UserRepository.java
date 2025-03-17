eec3ce38ab57b9cd138c01f7d93932722cc252ba
stromae <claudio.oliveira10.0p@gmail.com>
2025-03-17 16:32:27 -0300

    use of postgres and h2

--- src/main/java/br/com/net/sqlab_backend/domain_h2/repositories/postgres/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.net.sqlab_backend.domain_h2.repositories.postgres;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.net.sqlab_backend.domain_h2.models.postgres.Users;
-
-public interface UserRepository extends JpaRepository<Users, Long> {
-    
-}
