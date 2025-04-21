c313255287ecbc46d421993880e49ff92ac7cd74
stromae <claudio.oliveira10.0p@gmail.com>
2025-04-20 22:19:44 -0300

    criada feature resolver questão

--- src/app/pages/student/student-exercice/student-exercice.component.ts
+++ src/app/pages/student/student-exercice/student-exercice.component.ts
@@ -1,18 +1,42 @@
 import { AfterViewInit, Component } from '@angular/core';
 import * as ace from 'ace-builds';
+import { ExerciseService } from '../services/exercise.service';
+import { CommonModule } from '@angular/common';
+
+interface QueryExercise {
+  studentId: string;
+  exerciseId: string;
+  query: string;
+};
+
+interface ResponseExercise {
+  isCorrect: boolean;
+  resultQuery: Array<Record<string, any>>
+};
 
 @Component({
   selector: 'app-student-exercice',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './student-exercice.component.html',
   styleUrl: './student-exercice.component.css'
 })
 export class StudentExerciceComponent implements AfterViewInit {
   imageUrl = ''; // Se quiser usar uma imagem, coloque a URL aqui
   private editor: ace.Ace.Editor | undefined;
+  private queryExercise: QueryExercise = {"studentId": "1", "exerciseId": "3", "query": ""};
+  responseExercise!: ResponseExercise;
+
+  errorMessage: string = '';
+
+  constructor(
+    private exerciseService: ExerciseService
+  ) {}
 
   ngAfterViewInit(): void {
+    ace.config.set('basePath', 'https://unpkg.com/ace-builds@1.4.12/src-noconflict');
     const aceEditor = ace.edit('editor');
     aceEditor.setTheme('ace/theme/monokai');
     aceEditor.session.setMode('ace/mode/sql');
@@ -22,25 +46,39 @@ export class StudentExerciceComponent implements AfterViewInit {
       showPrintMargin: false
     });
 
+    aceEditor.setValue("SELECT * FROM users WHERE age > 30;");
+
     this.editor = aceEditor;
   }
 
   getEditorValue(): string {
-    return  this.editor?.getValue() || '';
+    return this.editor?.getValue() || '';
+  }
+
+  getTableKeys(): string[] {
+    if (this.responseExercise && this.responseExercise.resultQuery.length > 0) {
+      return Object.keys(this.responseExercise.resultQuery[0]);
+    }
+    return [];
   }
 
   enviarResposta(): void {
     const respostaSQL = this.editor?.getValue() || '';
     console.log('Resposta enviada:', respostaSQL);
-  
-    // this.http.post('/api/respostas', { resposta: respostaSQL }).subscribe(...)
+
     if (!respostaSQL.trim()) {
       alert('Por favor, escreva uma resposta antes de enviar.');
       return;
     }
-  
-    // Enviar resposta (aqui só mostramos no console)
-    console.log('Resposta enviada:', respostaSQL);
-    alert('Resposta enviada com sucesso!');
+    this.queryExercise.query = respostaSQL
+    this.exerciseService.sentAnswer(this.queryExercise).subscribe({
+      next: (data: ResponseExercise) => {
+        this.responseExercise = data;
+      },
+      error: (err: any) => {
+        this.errorMessage = err;
+      }
+    });
   }
+
 }
