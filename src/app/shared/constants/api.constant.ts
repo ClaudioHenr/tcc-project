184b251bb13aff7105a60ff435ea046fd201e858
israelidq <49970222+israelidq@users.noreply.github.com>
2025-05-02 22:40:02 -0300

    Login, Token, Roles, Cadastro de Aluno e Professor

---
+++ src/app/shared/constants/api.constant.ts
@@ -0,0 +1,9 @@
+export const API_CONFIG = {
+    auth: {
+      login: '/auth/signin',
+      register: {
+        professor: '/professors/create',
+        student: '/students/create'
+      }
+    },
+  };
